b800d5393e725a07463c9179630b482d73c91b18
Leticia <leticiatp2@gmail.com>
2022-10-22 13:41:16 -0300

    Sing-up finalizado

--- sing-up/script.js
+++ sing-up/script.js
@@ -1,11 +1,43 @@
 function validaCampos(){
-    var nome = document.getElementById('nome').value
-    var ultNome = document.getElementById('ultNome').value
-    var email = document.getElementById('email').value
-    var senha = document.getElementById('senha').value
-    
-    console.log(nome)
-    if(nome == null || nome == ''){
-        //nome.style.borderColor = 'red'
+    var nome = document.getElementById('nome')
+    var ultNome = document.getElementById('ultNome')
+    var email = document.getElementById('email')
+    var senha = document.getElementById('senha')
+    var warnings = document.querySelectorAll('.warnings')
+
+    console.log(warnings)
+
+    if(nome.value.length == ''){
+        nome.style.borderColor = 'hsl(0, 100%, 74%)'
+        warnings[0].style.display = 'block'
+    } else {
+        nome.style.borderColor = 'hsl(154, 59%, 51%)'
+        warnings[0].style.display = 'none'
     }
+    if(ultNome.value.length == ''){
+        ultNome.style.borderColor = 'hsl(0, 100%, 74%)'
+        warnings[1].style.display = 'block'
+    } else {
+        ultNome.style.borderColor = 'hsl(154, 59%, 51%)'
+        warnings[1].style.display = 'none'
+    }
+    if(email.value.length == ''){
+        email.style.borderColor = 'hsl(0, 100%, 74%)'
+        warnings[2].style.display = 'block'
+    } else {
+        email.style.borderColor = 'hsl(154, 59%, 51%)'
+        warnings[2].style.display = 'none'
+    }
+    if(senha.value.length == ''){
+        senha.style.borderColor = 'hsl(0, 100%, 74%)'
+        warnings[3].style.display = 'block'
+    }  else {
+        senha.style.borderColor = 'hsl(154, 59%, 51%)'
+        warnings[3].style.display = 'none'
+    }
+
+    nome.value = ''
+    ultNome.value = ''
+    email.value = ''
+    senha.value = ''
 }
