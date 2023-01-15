61a6667ea53431708a025b5480542d401fa383a6
Leticia <leticiatp2@gmail.com>
2023-01-15 16:26:20 -0300

    Adicionando um pequeno script de validação

--- sing-up-form/script.js
+++ sing-up-form/script.js
@@ -1,30 +1,7 @@
-const cadastros = {
-    "usuarios":[
-        {
-            "id": 1,
-            "email": "leticia@gmail.com",
-            "senha": "123456"
-        },
-        {
-            "id": 2,
-            "email": "fluffy@gmail.com",
-            "senha": "aiai"
-        }        
-    ]
-};
-
 function verificaLogin(){
     let emailUsuario = document.getElementById('email');
-    let senhaUsuario = document.getElementById('senha');
-    let usuarioValido = false;
+    let senhaUsuario = document.getElementById('senha');    
 
-    cadastros.usuarios.forEach((user) =>{        
-        if(user.email == emailUsuario.value && user.senha == senhaUsuario.value){
-            usuarioValido = true;
-            console.log("passsei", usuarioValido);
-        } 
-    })
-
-    if(usuarioValido) alert("Usuário Válido!");
+    if(emailUsuario.value == 'leticia@gmail.com' && senhaUsuario.value == '1234') alert("Usuário Válido!");
     else alert("Usuário inválido! Email ou senha incorretos.");
 }
